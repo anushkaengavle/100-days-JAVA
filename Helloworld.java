4c39abb3fbfcc23c682fb06f62ceb519be66bc9f
Anushka Engavle <83860689+anushkaengavle@users.noreply.github.com>
2021-10-22 20:00:30 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+/*
+    Print 'Hello World' in console and run the program
+*/
+
+public class Helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
---
+++ printing_dif_put.java
@@ -0,0 +1,15 @@
+
+/*
+    Take a integer, decimal, string & character as inputs and print them out.
+*/
+
+public class printing_dif_put {
+    public static void main(String[] args) {
+
+        int a=2;
+        double b=2.5;
+        String ch="Anushka";
+        char d= 'a';
+System.out.println("integer = "+a+"\ndecimal = "+d+"\nString = "+ch+"\ncharacter = "+d);
+    }
+}
